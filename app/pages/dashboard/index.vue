c128d0e6413e0d6e8545fc1e0ee55462d259587c
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2024-10-02 12:14:00 -0400

    Refactor dashboard view options and create database button in index.vue

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -12,24 +12,20 @@
         <template #description></template>
         <template #links>
           <UPopover>
-            <UButton
-              icon="material-symbols:responsive-layout-outline-rounded"
-              label="Layout"
-              color="gray"
-              class="px-2 py-[5.8px] top-0 relative"
-              :popper="{ placement: 'bottom-start' }"
-            />
+            <UButton icon="material-symbols:responsive-layout-outline-rounded" label="Layout" color="gray"
+              class="px-2 py-[5.8px] top-0 relative" :popper="{ placement: 'bottom-start' }" />
             <template #panel>
-              <div class="overflow-hidden focus:outline-none bg-white dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 rounded-lg shadow-sm">
+              <div
+                class="overflow-hidden focus:outline-none bg-white dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700 rounded-lg shadow-sm">
                 <div class="px-2 pt-2 pb-2 border border-gray-200 dark:border-gray-700">
                   <div class="grid grid-cols-2 gap-2">
                     <!-- Card View Button -->
                     <button
                       class="relative flex flex-col items-center px-4 py-3 transition-colors duration-100 rounded shadow-sm ring-1 ring-inset focus:outline-none ring-gray-200 dark:ring-gray-700 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700/25 dark:hover:bg-gray-700/50 u-text-gray-500 hover:u-text-gray-700"
                       :class="{ 'ring-1 dark:ring-gray-500 ring-gray-400': currentView.__name === 'homeCard' }"
-                      @click="setView('card')"
-                    >
-                      <UIcon name="material-symbols:responsive-layout-outline-rounded" class="flex-shrink-0 w-5 h-5 mb-1" />
+                      @click="setView('card')">
+                      <UIcon name="material-symbols:responsive-layout-outline-rounded"
+                        class="flex-shrink-0 w-5 h-5 mb-1" />
                       <span class="text-[12px] font-medium">Card</span>
                     </button>
 
@@ -37,8 +33,7 @@
                     <button
                       class="relative flex flex-col items-center px-4 py-3 transition-colors duration-100 rounded shadow-sm ring-1 ring-inset focus:outline-none ring-gray-200 dark:ring-gray-700 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700/25 dark:hover:bg-gray-700/50 u-text-gray-500 hover:u-text-gray-700"
                       :class="{ 'ring-1 dark:ring-gray-500 ring-gray-400': currentView.__name === 'homeTable' }"
-                      @click="setView('table')"
-                    >
+                      @click="setView('table')">
                       <UIcon name="material-symbols:view-list-outline-rounded" class="flex-shrink-0 w-5 h-5 mb-1" />
                       <span class="text-[12px] font-medium">Table</span>
                     </button>
@@ -47,18 +42,14 @@
               </div>
             </template>
           </UPopover>
-          <UButton
-            icon="mdi:database-plus-outline"
-            label="Create Database"
-            color="gray"
-            @click="modal.open(LazyDashboardCreateDatabase)"
-          />
+          <UButton icon="mdi:database-plus-outline" label="Create Database" color="gray"
+            @click="modal.open(LazyDashboardCreateDatabase)" />
         </template>
       </UDashboardCard>
 
       <!-- Conditional Rendering of Card or Table view -->
       <component v-bind:is="currentView" />
-      
+
     </UDashboardPanel>
   </UDashboardPage>
 </template>
